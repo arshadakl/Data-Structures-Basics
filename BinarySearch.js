c5280f8171a21756c7acae7846f81c567c4f5eb8
arshadakl <arshadayanikkal@gmail.com>
2023-12-06 21:22:58 +0530

    Binary search added

---
+++ BinarySearch.js
@@ -0,0 +1,19 @@
+function searchBinary(arr,target){
+    let left = 0
+    let right = arr.length-1
+    while(left<=right){
+        let mid = Math.floor((left+right)/2)
+        if(target===arr[mid]){
+            return mid
+        }
+        if(target < arr[mid]){
+            right=mid-1
+        }else{
+            left= mid+1
+        }
+    }
+    return -1
+}
+
+const arr = [-5,2,4,6,10];
+console.log(searchBinary(arr,10));
